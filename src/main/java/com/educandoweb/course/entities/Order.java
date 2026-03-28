9ff6061fe7e7d924e8e023eb3fe955ac6aa0c31f
Vitor Pereira <vitor.pereira8@hotmail.com>
2026-03-28 10:31:52 -0300

    Subtotal & Total methods

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -85,6 +85,14 @@ public class Order implements Serializable {
         }
     }
 
+    public Double getTotal(){
+        double sum = 0.0;
+        for (OrderItem x : items){
+            sum += x.getSubTotal();
+        }
+        return sum;
+    }
+
     public User getClient() {
         return client;
     }
